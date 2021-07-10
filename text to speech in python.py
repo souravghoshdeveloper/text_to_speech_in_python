c67e6a8c0efd93c172a4b8624991fc2ea1d1dfac
Sourav Ghosh <infosourav04@gmail.com>
2021-07-10 10:10:34 +0530

    Initial commit

---
+++ text to speech in python.py
@@ -0,0 +1,23 @@
+import pyttsx3
+speaker = pyttsx3.init()
+speaker.say("""
+You're the light, you're the night
+You're the color of my blood
+You're the cure, you're the pain
+You're the only thing I wanna touch
+Never knew that it could mean so much, so much
+You're the fear, I don't care
+'Cause I've never been so high
+Follow me through the dark
+Let me take you past our satellites
+You can see the world you brought to life, to life
+So love me like you do, lo-lo-love me like you do
+Love me like you do, lo-lo-love me like you do
+Touch me like you do, to-to-touch me like you do
+What are you waiting for?
+Fading in, fading out
+On the edge of paradise
+Every inch of your skin, is a Holy Grail I've gotta find
+Only you can set my heart on fire, on…
+""")
+speaker.runAndWait()
